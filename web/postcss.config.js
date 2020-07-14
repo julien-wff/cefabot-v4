686917437c5875b2c5e241ea86cd786135c82025
cefadrom <cefadrom1@gmail.com>
2020-07-14 18:36:41 +0200

    Added autoprefixer and cssnano

--- web/postcss.config.js
+++ web/postcss.config.js
@@ -3,6 +3,8 @@ const tailwindcss = require('tailwindcss');
 module.exports = {
     plugins: [
         tailwindcss('./tailwind.config.js'),
-        require('postcss-easy-import')
+        require('postcss-easy-import'),
+        require('autoprefixer'),
+        ...(process.env.NODE_ENV === 'production' ? [require('cssnano')] : [])
     ],
 };
