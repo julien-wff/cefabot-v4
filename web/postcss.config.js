39c0d793f9af492949a91c85345e5f893f7b9ee8
cefadrom <cefadrom1@gmail.com>
2020-10-08 11:27:17 +0200

    Updated dependencies (introduced a bug with tailwind)

--- web/postcss.config.js
+++ web/postcss.config.js
@@ -4,7 +4,7 @@ module.exports = {
     plugins: [
         tailwindcss('./tailwind.config.js'),
         require('postcss-easy-import'),
-        require('autoprefixer'),
+        // require('autoprefixer'),
         ...(process.env.NODE_ENV === 'production' ? [require('cssnano')] : [])
     ],
 };
