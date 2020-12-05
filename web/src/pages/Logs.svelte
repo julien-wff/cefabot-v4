522a5bd13d4433c476962586df9d1829347ff14e
Julien <cefadrom1@gmail.com>
2020-12-05 15:36:39 +0100

    Synced the app and bots filters with the URL search params

--- web/src/pages/Logs.svelte
+++ web/src/pages/Logs.svelte
@@ -1,16 +1,17 @@
 <script>
-    import Options from '../components/logs/Options.svelte';
-    import { setContext, onDestroy, onMount } from 'svelte';
-    import LogLine from '../components/logs/LogLine.svelte';
+    import { onDestroy, onMount, setContext } from 'svelte';
     import { writable } from 'svelte/store';
+    import LogLine from '../components/logs/LogLine.svelte';
+    import Options from '../components/logs/Options.svelte';
 
     let socket;
 
     onMount(() => {
         socket = new WebSocket(`ws://${window.location.host}/ws/`);
         socket.addEventListener('message', msg => {
-            if (msg.data === 'new-log')
-                refresh();
+            if (msg.data === 'new-log') {
+                getBots().then(getLogs);
+            }
         });
     });
 
@@ -46,7 +47,6 @@
     async function getBots() {
         const req = await fetch('/api/bots');
         $bots = await req.json();
-        return $bots;
     }
 
     async function getLogs() {
@@ -59,13 +59,42 @@
         $logs = await res.json();
     }
 
-    async function refresh() {
-        const bots = await getBots();
-        $filters = { ...$filters, bots: bots.map(b => b.id) };
-        await getLogs();
+    function updateURLSearchParams() {
+        let params = '?';
+        if ($filters.app)
+            params += 'app';
+        if (Array.isArray($filters.bots) && $filters.bots.length > 0) {
+            if (params !== '?')
+                params += '&';
+            params += `bots=${$filters.bots.join(',')}`;
+        }
+        window.history.replaceState(null, null, params);
     }
 
-    refresh();
+    const filterBotsArray = arr => {
+        // Only keep the IDs that are valid (= linked to an existing bot)
+        let newArr = arr.filter(b => $bots.find(existingBot => existingBot.id === b));
+        // Remove duplicated IDs
+        return [...new Set(newArr)];
+    };
+
+
+    onMount(async () => {
+        await getBots();
+
+        const URLFilters = new URLSearchParams(window.location.search || 'app');
+        $filters.app = URLFilters.get('app') !== null;
+        const bots = URLFilters.get('bots');
+        if (typeof bots === 'string' && bots.match(/^[0-9a-fA-F]{24}(,[0-9a-fA-F]{24})*$/)) {
+            let botsArray = bots.split(/,/g);
+            botsArray = filterBotsArray(botsArray);
+            console.log(botsArray);
+            $filters.bots = botsArray;
+        }
+
+        filters.subscribe(updateURLSearchParams);
+        await getLogs();
+    });
 </script>
 
 <div class="md:mx-10 md:mt-10 sm:mx-4 sm:mt-4 m-2 p-4 bg-gray-700 rounded">
